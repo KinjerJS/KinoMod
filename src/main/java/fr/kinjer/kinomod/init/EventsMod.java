017f7e192c70ae01f22052afef4e4e9bd9853101
Kinjer <67011274+KinjerJS@users.noreply.github.com>
2021-07-08 22:58:07 +0200

    Fix armor bismuth

--- src/main/java/fr/kinjer/kinomod/init/EventsMod.java
+++ src/main/java/fr/kinjer/kinomod/init/EventsMod.java
@@ -1,8 +1,6 @@
 package fr.kinjer.kinomod.init;
 
 import fr.kinjer.kinomod.items.ItemArmorKino;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -10,11 +8,8 @@ public class EventsMod {
 	
 	@SubscribeEvent
 	public void isOnFire(LivingDamageEvent e) {
-		System.out.println(e.getSource().getDamageType());
-		if(e.getEntity() instanceof EntityPlayer && ItemArmorKino.isFullArmor((EntityPlayer) e.getEntity()))
-		if(e.getSource() == DamageSource.ON_FIRE || e.getSource() == DamageSource.IN_FIRE || e.getSource() == DamageSource.LAVA) {
-			e.setCanceled(true);
-		}
+		
+		ItemArmorKino.onPlayerAttacked(e);
 	}
 
 }
--- src/main/java/fr/kinjer/kinomod/items/ItemArmorKino.java
+++ src/main/java/fr/kinjer/kinomod/items/ItemArmorKino.java
@@ -1,5 +1,8 @@
 package fr.kinjer.kinomod.items;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import fr.kinjer.kinomod.KinoMod;
 import fr.kinjer.kinomod.init.ItemsMod;
 import net.minecraft.entity.SharedMonsterAttributes;
@@ -12,27 +15,42 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemArmorKino extends ItemArmor {
+	
+	private static final Set<String> damageNegations = new HashSet<>();
 	
 	public ItemArmorKino(String name, ArmorMaterial material, int renderIndex, EntityEquipmentSlot equipmentSlot) {
 		super(material, renderIndex, equipmentSlot);
 		setCreativeTab(KinoMod.tabKino);
+		
+		damageNegations.add(DamageSource.DROWN.damageType);
+		damageNegations.add(DamageSource.FALL.damageType);
+		damageNegations.add(DamageSource.LAVA.damageType);
+		damageNegations.add(DamageSource.IN_WALL.damageType);
+		damageNegations.add(DamageSource.STARVE.damageType);
+		damageNegations.add(DamageSource.IN_FIRE.damageType);
+		damageNegations.add(DamageSource.ON_FIRE.damageType);
+		damageNegations.add(DamageSource.HOT_FLOOR.damageType);
+		damageNegations.add(DamageSource.FLY_INTO_WALL.damageType);
+		
 		ItemsMod.itemsarmor.add((ItemArmor) this.setRegistryName(KinoMod.MODID, name).setUnlocalizedName(KinoMod.MODID + "." + name));
 	}
 
 	@Override
 	public void onArmorTick(World world, EntityPlayer player, ItemStack stack) {
 		if(isFullArmor(player)) {
+			if (player.isBurning()) {
+				player.extinguish();
+			}
 			player.capabilities.allowFlying = true;
 			player.stepHeight = 1.0625F;
 			player.fallDistance = 0.0f;
 			player.capabilities.setFlySpeed(0.15F);
 			player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(80);
 			player.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(1.0D);
-			player.setFire(0);
 			player.addPotionEffect(new PotionEffect(MobEffects.NIGHT_VISION, 20*13,0,false,false));
+			
 		}else {
 			player.stepHeight = 0.5F;
 			player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20);
@@ -44,6 +62,17 @@ public class ItemArmorKino extends ItemArmor {
 			}
 		}
 		
+	}
+	
+	public static void onPlayerAttacked(LivingDamageEvent event) {
+		if (event.getEntity() instanceof EntityPlayer) {
+			System.out.println("ui");
+			EntityPlayer player = (EntityPlayer) event.getEntity();
+			if (damageNegations.contains(event.getSource().damageType)
+					&& isFullArmor(player)) {
+				event.setCanceled(true);
+			}
+		}
 	}
 	
 	public static boolean isFullArmor(EntityPlayer player) {
